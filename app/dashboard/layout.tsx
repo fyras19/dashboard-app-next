e9f863328b3fd848fa0ee4d94370091ed1fcc67c
Firas Yahyaoui <firas.yahyaoui@imt-atlantique.net>
2024-10-19 15:48:48 +0200

    Add partial prerendering

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
 
+export const experimental_ppr = true;
+
 const Layout = ({ children }: { children: React.ReactNode }) => (
   <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
     <div className="w-full flex-none md:w-64">
